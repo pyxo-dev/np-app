9de5c926fef20654beee4bc1c7967a5cba5312bd
Ejez <ejez@protonmail.ch>
2021-06-14 21:02:01 +0300

    Edit full loader component.

--- src/components/main/np-full-loader.ts
+++ src/components/main/np-full-loader.ts
@@ -1,10 +1,46 @@
-import { html, LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
-import styles from './np-full-loader.css.js';
 
 @customElement('np-full-loader')
 export class NpFullLoader extends LitElement {
-  static styles = styles;
+  static styles = css`
+    :host {
+      display: flex;
+      height: 100vh;
+    }
+
+    #spinner {
+      margin: auto;
+
+      border: 6px solid #737373;
+      border-radius: 50%;
+      border-top: 6px solid #353535;
+      width: 40px;
+      height: 40px;
+
+      -webkit-animation: spin 2s linear infinite; /* Safari */
+      animation: spin 2s linear infinite;
+    }
+
+    /* Safari */
+    @-webkit-keyframes spin {
+      0% {
+        -webkit-transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+      }
+    }
+
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `;
 
   render() {
     return html`<div id="spinner"></div>`;

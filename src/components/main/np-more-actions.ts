6ba665fd41395becd5f2c77d4c1609b2935d70a7
Ejez <ejez@protonmail.ch>
2021-06-16 17:01:27 +0300

    Simplify "more actions" component.

--- src/components/main/np-more-actions.ts
+++ src/components/main/np-more-actions.ts
@@ -1,44 +1,24 @@
-import { ActionBar } from '@spectrum-web-components/action-bar';
 import '@spectrum-web-components/action-bar/sp-action-bar.js';
 import '@spectrum-web-components/action-button/sp-action-button.js';
-import { css, html, LitElement } from 'lit';
+import '@spectrum-web-components/icons-workflow/icons/sp-icon-more.js';
+import '@spectrum-web-components/overlay/overlay-trigger.js';
+import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
-import { query } from 'lit/decorators/query.js';
 import './np-more-actions-items.js';
 
 @customElement('np-more-actions')
 export class NpMoreActions extends LitElement {
-  static styles = css`
-    sp-action-bar {
-      position: absolute;
-      top: 100%;
-    }
-    sp-action-bar[dir='ltr'] {
-      right: 0;
-    }
-    sp-action-bar[dir='rtl'] {
-      left: 0;
-    }
-  `;
-
-  @query('sp-action-bar')
-  actionBar: ActionBar | undefined;
-
-  private _toggleBar() {
-    if (this.actionBar) this.actionBar.open = !this.actionBar.open;
-  }
-
   render() {
     return html`
-      <div>
-        <sp-action-button quiet @click=${this._toggleBar}>
-          ...
+      <overlay-trigger>
+        <sp-action-button slot="trigger" quiet>
+          <sp-icon-more></sp-icon-more>
         </sp-action-button>
 
-        <sp-action-bar>
+        <sp-action-bar slot="click-content">
           <np-more-actions-items></np-more-actions-items>
         </sp-action-bar>
-      </div>
+      </overlay-trigger>
     `;
   }
 }

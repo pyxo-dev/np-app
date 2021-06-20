6291452e81b55e0e10f8f36dc772463c48a360dc
Ejez <ejez@protonmail.ch>
2021-06-20 11:49:05 +0300

    Edit "more actions" component.

--- src/sc/sc-more-actions.ts
+++ src/sc/sc-more-actions.ts
@@ -1,24 +1,41 @@
 import '@spectrum-web-components/action-bar/sp-action-bar.js';
 import '@spectrum-web-components/action-button/sp-action-button.js';
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-more.js';
-import '@spectrum-web-components/overlay/overlay-trigger.js';
-import { html, LitElement } from 'lit';
+import '@spectrum-web-components/underlay/sp-underlay.js';
+import { css, html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
+import { property } from 'lit/decorators/property.js';
 import './sc-more-actions-items.js';
 
 @customElement('sc-more-actions')
 export class ScMoreActions extends LitElement {
+  static styles = css`
+    sp-action-bar {
+      position: absolute;
+      top: 50%;
+      inset-inline-end: 0;
+  `;
+
+  @property({ type: Boolean }) open = false;
+
+  private show() {
+    this.open = true;
+  }
+
+  private hide() {
+    this.open = false;
+  }
+
   render() {
     return html`
-      <overlay-trigger>
-        <sp-action-button slot="trigger" quiet>
-          <sp-icon-more></sp-icon-more>
-        </sp-action-button>
+      <sp-underlay ?open=${this.open} @click=${this.hide}></sp-underlay>
+      <sp-action-button @click=${this.show} quiet>
+        <sp-icon-more></sp-icon-more>
+      </sp-action-button>
 
-        <sp-action-bar slot="click-content">
-          <np-more-actions-items></np-more-actions-items>
-        </sp-action-bar>
-      </overlay-trigger>
+      <sp-action-bar ?open=${this.open}>
+        <sc-more-actions-items></sc-more-actions-items>
+      </sp-action-bar>
     `;
   }
 }
